f344b3f3681f571baa0dfa86e59bc7fe6d9497ed
Dmitry Nevada <vegnerdima@yandex.ru>
2019-02-14 01:30:17 +0700

    Script finished
    JS-code optimizing required;
    .active css-property's fix required;
    Minor fixes required in pixel-perfect;

--- js/script.js
+++ js/script.js
@@ -1,20 +1,50 @@
 $(function() {
 	$('.menu-toggler').on('click', function() {
-		$('.menu-togglable').slideToggle(200, function() {
-			if($(this).css('display') === 'none') {
-				$(this).removeAttr('style');
-			}
-		});
+		if ($(document).width() <= 480) {
+			// Деактивация активных элементов при наличии таковых
+			$('.active').find('.sub-menu').slideToggle(0);
+			$('.active').removeClass('active');
+
+			// Переключение меню
+			$('.menu-togglable').slideToggle(200, function() {
+				if($(this).css('display') === 'none') {
+					$(this).removeAttr('style');
+				}
+			});
+		}
 	});
 
 	$('.header-nav-item').on('click', function() {
-		if($(this).hasClass('active')) {
-			$(this).removeClass('active');
-			$(this).find('.sub-menu').slideToggle(250);
+		if ($(document).width() <= 480) {
+			if($(this).hasClass('active')) {
+				// Деактивация задействованного элемента
+				$(this).find('.sub-menu').slideToggle(200);
+				$(this).removeClass('active');
+			}
+			else {
+				// Деактивация других активных элементов при наличии таковых
+				$('.active').find('.sub-menu').slideToggle(200);
+				$('.active').removeClass('active');
+				// Активация задействованного элемента
+				$(this).find('.sub-menu').slideToggle(200);
+				$(this).addClass('active');
+			}
 		}
-		else {
-			$(this).addClass('active');
-			$(this).find('.sub-menu').slideToggle(250);
+	});
+
+	$(window).resize(function() {
+		// Если окно увеличилось, а меню оставалось открытым - уберём его
+		if($(document).width() > 480 && $('.menu-togglable').css('display') === 'block') {
+			// Деактивация активных элементов при наличии таковых
+			$('.active').find('.sub-menu').slideToggle(0);
+			$('.active').removeClass('active');
+
+			// Переключение меню
+			$('.menu-togglable').slideToggle(200, function() {
+				if($(this).css('display') === 'none') {
+					$(this).removeAttr('style');
+				}
+			});
 		}
 	});
 });
