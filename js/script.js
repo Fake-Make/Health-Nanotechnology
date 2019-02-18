7df27c6e699c8cdb9a39e39b1acd27863484fdd7
Dmitry Nevada <vegnerdima@yandex.ru>
2019-02-18 11:08:57 +0700

    Flex-display-fixed changed to optimized

--- js/script.js
+++ js/script.js
@@ -18,6 +18,21 @@ function menuToggle(item) {
 	});
 }
 
+// Функция для исправления отображения флекс-бокса
+function flexFix() {
+	if($(document).width() > 480)
+		var n = 3;
+	else var n = 2;
+
+	// Если количество элементов не кратно n, то дополняется до кратности n
+	if($('ul.categories').children().length % n) {
+		// Перед запуском удаляются лишние фикс-элементы
+		$('.category.hidden').remove();
+		while($('ul.categories').children().length % n)
+			$('ul.categories').append('<li class="category hidden"><a><img><span></span></a></li>');
+	}
+}
+
 // Когда документ загрузился, добавим функции элементам
 $(function() {
 	// Переключатель основного меню
@@ -51,28 +66,17 @@ $(function() {
 		}
 	});
 
+	// Починка флекс-отображения категорий при загрузке страницы
+	flexFix();
+
 	// Закрывашка меню при смене области просмотра Mobile -> Desktop
 	$(window).resize(function() {
-		// Функция для исправления отображения флекс-бокса
-		function flexFix(n) {
-		// Если количество элементов не кратно n, то дополняется до кратности n
-			if($('ul.categories').children().length % n) {
-				// Перед запуском удаляются лишние фикс-элементы
-				$('.category.hidden').remove();
-				while($('ul.categories').children().length % n)
-					$('ul.categories').append('<li class="category hidden"><a><img><span></span></a></li>');
-			}
-		}
-
 		if($(document).width() > 480) {
 			if($('.menu-togglable').css('display') === 'flex') {
 				menuToggle('.menu-togglable');
 			}
+		}
 
-			flexFix(3);
-		}
-		if($(document).width() < 481) {
-			flexFix(2);
-		}
+		flexFix();
 	});
 });
