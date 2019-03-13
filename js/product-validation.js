d6563e1610b7bc5f08fe1138600562c796c0a3a7
Dmitry Nevada <vegnerdima@yandex.ru>
2019-03-13 18:33:54 +0700

    JS compiling in script.js; Contacts.html reconstructed: classes in error-messages changed from unique to common; [0].value replaced by .val(); Minor fixes;

--- js/product-validation.js
+++ js/product-validation.js
@@ -4,27 +4,27 @@ $(function () {
 	$('.amount-tumbler__button_left').on('click', function () {
 		var a = $(this).siblings('.products-amount__input')
 		if (!(a.val() >= 1) || a.val() % 1 != 0)
-			a[0].value = 1;
+			a.val(1);
 
 		if (a.val() > 1)
-			a[0].value = parseInt(a.val(), 10) - 1;
+			a.val(+a.val() - 1);
 	});
 
 	// Действие кнопки увеличения количества товара
 	// параллельно с валидацией
 	$('.amount-tumbler__button_right').on('click', function () {
 		var a = $(this).siblings('.products-amount__input')
-		if (!(a.val() >= 1) || a.val() % 1 != 0)
-			a[0].value = 1;
+		if (!(parseInt(a.val()) >= 1) || a.val() % 1 != 0)
+			a.val(1);
 
-		a[0].value = parseInt(a.val(), 10) + 1;
+		a.val(+a.val() + 1);
 	});
 
 	// Валидация при потере фокуса
 	$('.products-amount__input').focusout(function () {
 		var amount = $(this).val();
 		if (!(amount >= 1) || amount % 1 != 0)
-			$(this)[0].value = 1;
+			$(this).val(1);
 	});
 
 	// Валидация при отправке формы
--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 const phoneSize = 480;
 
+// Функция для исправления отображения флекс-бокса
+function flexFix() {
+	if ($(window).width() > phoneSize)
+		var n = 3;
+	else
+		var n = 2;
+
+	// Если количество элементов не кратно n, то дополняется до кратности n
+	if ($('ul.categories').children().length % n) {
+		// Перед запуском удаляются лишние фикс-элементы
+		$('.category.hidden').remove();
+		while ($('ul.categories').children().length % n)
+			$('ul.categories').append('<li class="category hidden"></li>');
+	}
+}
+
 // Деактивация активных элементов при наличии таковых
 function hideAllActive() {
 	$('.active').removeClass('active').find('.sub-menu').css('display', 'none');
@@ -19,53 +35,43 @@ function menuToggle(item) {
 function sidebarHide() {
 	if ($(window).width() <= phoneSize) {
 		if ($(document).find('.header-nav-item__link_current').is('.header-nav-item__link_current')) {
-			if ($('.header-nav-item__link_current')[0].innerText != "Главная") {
+			if ($('.header-nav-item__link_current').text() != "Главная") {
 				$('.sidebar').css('display', 'none');
 			}
 		}
 	}
 }
 
-// Когда документ загрузился, добавим функции элементам
+// Функция валидации поля
+function validate(item) {
+	if ($(item).val() === "") {
+		// Если пусто, то показываем сообщение об ошибке, делаем границы поля красными
+		$(item).next('.error-emptyness').removeClass('invisible');
+		$(item).addClass('incorrect-input-style');
+		return 1;
+	} else {
+		// Иначе скрываем сообщение, возвращаем цвета границ
+		$(item).next('.error-emptyness').addClass('invisible');
+		$(item).removeClass('incorrect-input-style');
+		return 0;
+	}
+}
+
+// Функция привязки валидаторов к полям	
+function validateForEmptyness(form, input) {
+	$(form + ' ' + input).focusout(function () {
+		validate(this);
+	});
+}
+
+// Назначение элементам событий после загрузки документа
 $(function () {
-	// Переключатель основного меню
-	$('.menu-toggler').on('click', function () {
-		if ($(window).width() <= phoneSize) {
-			menuToggle('.menu-togglable');
-		}
-	});
-
-	// Переключатель под-меню
-	$('.header-nav-item').on('click', function () {
-		if ($(window).width() <= phoneSize) {
-			if ($(this).hasClass('active')) {
-				// Деактивация задействованного элемента
-				hideAllActive();
-			} else {
-				// Деактивация других активных элементов при наличии таковых
-				hideAllActive();
-				// Активация задействованного элемента
-				//menuToggle('.sub-menu');
-				if ($(this).find('.sub-menu').is('ul')) {
-					menuToggle('.sub-menu');
-					$(this).addClass('active');
-					return false;
-				}
-			}
-		}
-	});
-
+	// Починка флекс-отображения категорий при загрузке страницы
+	flexFix();
 	// Скрытие сайдбара при просмотре Mobile на внутренних страницах
 	sidebarHide();
 
-	// Заливка непустых полей ввода
-	$('.user-info__input').on('change', function () {
-		if ($(this).val() === "")
-			$(this).removeClass('user-info__input_filled-white')
-		else
-			$(this).addClass('user-info__input_filled-white')
-	})
-
+	// Настройка отображения при изменении размеров экрана
 	$(window).resize(function () {
 		if ($(window).width() > phoneSize) {
 			// Закрывашка меню при смене области просмотра Mobile -> Desktop
@@ -80,4 +86,97 @@ $(function () {
 		}
 		sidebarHide();
 	});
+
+	{	// Общие настройки для всего сайта
+		// Переключатель основного меню
+		$('.menu-toggler').on('click', function () {
+			if ($(window).width() <= phoneSize) {
+				menuToggle('.menu-togglable');
+			}
+		});
+
+		// Переключатель под-меню
+		$('.header-nav-item').on('click', function () {
+			if ($(window).width() <= phoneSize) {
+				if ($(this).hasClass('active')) {
+					// Деактивация задействованного элемента
+					hideAllActive();
+				} else {
+					// Деактивация других активных элементов при наличии таковых
+					hideAllActive();
+					// Активация задействованного элемента
+					//menuToggle('.sub-menu');
+					if ($(this).find('.sub-menu').is('ul')) {
+						menuToggle('.sub-menu');
+						$(this).addClass('active');
+						return false;
+					}
+				}
+			}
+		});
+
+		// Заливка непустых полей ввода
+		$('.user-info__input').on('change', function () {
+			if ($(this).val() === "")
+				$(this).removeClass('user-info__input_filled-white')
+			else
+				$(this).addClass('user-info__input_filled-white')
+		})
+	}
+
+	{	// Группа специфических настроек
+		// Очистка полей при нажатии соответствующей кнопки
+		// Кнопка ничего не сделает без JavaScript
+		$('form[name="contats-page__feedback-form"] .clear-inputs').on('click', function () {
+			// Удаление значений всех полей, кроме кнопок и сабмитов
+			// И возвращение изначального цвета границ полей
+			$('form[name="contats-page__feedback-form"]').find('input[type != "submit"][type != "button"], textarea[type="textarea"]').each(function (i, item) {
+				$(item).val("");
+				$(item).removeClass('incorrect-input-style');
+			});
+			// А так же скрытие всех сообщений об ошибках
+			// Мы же не хотим пугать пользователя ещё до ввода информации
+			$('.error-emptyness').addClass('invisible');
+		});
+	}
+
+	{	// Секция валидации
+		{	// Группа функций для валидации полей при потере фокуса
+			// Валидация полей формы "Обратная связь" на странице "Контакты"
+			validateForEmptyness('form[name="contats-page__feedback-form"]', 'input[name="feedback-author"]');
+			validateForEmptyness('form[name="contats-page__feedback-form"]', 'input[name="email"]');
+			validateForEmptyness('form[name="contats-page__feedback-form"]', 'textarea[name="feedback-text"]');
+
+			// Валидация формы "Фильтр поиска" на странице "Каталог"
+			$('.search-filter .search-filter__input').focusout(function () {
+				var amount = parseFloat($(this).val(), 10);
+				// Если не число, пустая строка или меньше нуля, то заменить
+				if (!(amount >= 0))
+					$(this).val(0);
+			});
+		}
+		
+		{	// Группа функций для валидации полей при отправке формы
+			// Валидация формы "Обратная связь" на странице "Контакты"
+			$('.registration-form').on('submit', function () {
+				var flag = 
+					validate('form[name="contats-page__feedback-form"] input[name="feedback-author"]') +
+					validate('form[name="contats-page__feedback-form"] input[name="email"]') +
+					validate('form[name="contats-page__feedback-form"] textarea[name="feedback-text"]');
+				
+				// Отправляем форму, только если все требуемые поля валидны
+				if (flag)
+					return false;
+			})
+
+			// Валидация формы "Фильтр поиска" на странице "Каталог"
+			$('.search-filter').on('submit', function () {
+				var min = parseFloat($(this).find('.search-filter__input[name="cost-from"]').val());
+				var max = parseFloat($(this).find('.search-filter__input[name="cost-to"]').val());
+				// Проверка на пустоту, логичность и неотрицательость
+				if (!(min <= max) || min < 0 || max < 0)
+					return false;
+			})
+		}
+	}
 });
